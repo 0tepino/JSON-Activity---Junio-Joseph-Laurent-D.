529742b71ae005468bf1748d56671ce4b7098dcf
0tepino <22bgu1242_ms@psu.edu.ph>
2025-05-06 19:54:49 +0800

    Add project files.

---
+++ GroceryList/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace GroceryList
+{
+    public partial class Form1 : Form
+    {
+        private readonly string filePath = "C:\\Users\\laurent\\Desktop\\GroceryList\\ClassLibrary6\\jsonfiles\\shoppinglist.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProductsAsync();
+        }
+
+        private async void LoadProductsAsync()
+        {
+            List<Product> products = await JsonHelper.ReadJsonFromFileAsync(filePath);
+            listBoxProducts.Items.Clear();
+            foreach (var product in products)
+            {
+                listBoxProducts.Items.Add($"{product.Id}: {product.Name} - ₱{product.Price}");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Form2 addForm = new Form2(filePath);
+            addForm.FormClosed += (s, args) => LoadProductsAsync();
+            addForm.ShowDialog();
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxProducts.SelectedIndex;
+
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("Please select an item to delete.");
+                return;
+            }
+
+            List<Product> products = await JsonHelper.ReadJsonFromFileAsync(filePath);
+
+            if (selectedIndex >= 0 && selectedIndex < products.Count)
+            {
+                products.RemoveAt(selectedIndex);
+
+                for (int i = 0; i < products.Count; i++)
+                {
+                    products[i].Id = i + 1;
+                }
+
+                await JsonHelper.WriteJsonToFileAsync(filePath, products);
+
+                LoadProductsAsync();
+            }
+        }
+
+
+    }
+}
---
+++ GroceryList/Form2.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GroceryList
+{
+    partial class Form2
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnAddItem;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox listBoxItems;
+        private System.Windows.Forms.Button btnBack;
+
+        private void InitializeComponent()
+        {
+            txtName = new TextBox();
+            txtPrice = new TextBox();
+            btnAddItem = new Button();
+            btnSave = new Button();
+            listBoxItems = new ListBox();
+            label1 = new Label();
+            btnBack = new Button();
+            SuspendLayout();
+ 
+            txtName.BackColor = SystemColors.ButtonFace;
+            txtName.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            txtName.Location = new Point(41, 145);
+            txtName.Name = "txtName";
+            txtName.PlaceholderText = "Product Name";
+            txtName.Size = new Size(217, 27);
+            txtName.TabIndex = 0;
+
+            txtPrice.BackColor = SystemColors.ButtonFace;
+            txtPrice.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            txtPrice.Location = new Point(41, 178);
+            txtPrice.Name = "txtPrice";
+            txtPrice.PlaceholderText = "Price";
+            txtPrice.Size = new Size(80, 27);
+            txtPrice.TabIndex = 1;
+ 
+            btnAddItem.BackColor = SystemColors.ButtonFace;
+            btnAddItem.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            btnAddItem.Location = new Point(360, 306);
+            btnAddItem.Name = "btnAddItem";
+            btnAddItem.Size = new Size(94, 40);
+            btnAddItem.TabIndex = 2;
+            btnAddItem.Text = "Add";
+            btnAddItem.UseVisualStyleBackColor = false;
+            btnAddItem.Click += btnAddItem_Click;
+ 
+            btnSave.BackColor = SystemColors.ButtonFace;
+            btnSave.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            btnSave.Location = new Point(494, 306);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 40);
+            btnSave.TabIndex = 4;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+
+            listBoxItems.BackColor = SystemColors.Info;
+            listBoxItems.Location = new Point(292, 145);
+            listBoxItems.Name = "listBoxItems";
+            listBoxItems.Size = new Size(352, 144);
+            listBoxItems.TabIndex = 3;
+
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(173, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(327, 62);
+            label1.TabIndex = 5;
+            label1.Text = "Add Groceries";
+
+            btnBack.BackColor = SystemColors.ButtonFace;
+            btnBack.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
+            btnBack.Location = new Point(588, 111);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(56, 28);
+            btnBack.TabIndex = 6;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = false;
+            btnBack.Click += btnBack_Click;
+   
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(680, 380);
+            Controls.Add(label1);
+            Controls.Add(txtName);
+            Controls.Add(txtPrice);
+            Controls.Add(btnAddItem);
+            Controls.Add(listBoxItems);
+            Controls.Add(btnSave);
+            Controls.Add(btnBack);
+            Name = "Form2";
+            Text = "Add Groceries";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+        private Label label1;
+    }
+}
+
+
