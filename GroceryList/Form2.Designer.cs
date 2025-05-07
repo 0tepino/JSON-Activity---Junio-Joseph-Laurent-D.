41599a1b2696fcf5997bf2402edd9cd41d06bd71
0tepino <22bgu1242_ms@psu.edu.ph>
2025-05-07 22:37:25 +0800

    Update Form2.Designer.cs

--- GroceryList/Form2.Designer.cs
+++ GroceryList/Form2.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GroceryList
+namespace GroceryList
 {
     partial class Form2
     {
@@ -14,92 +14,80 @@
         }
 
         private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.TextBox txtPrice;
         private System.Windows.Forms.Button btnAddItem;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.ListBox listBoxItems;
-        private System.Windows.Forms.Button btnBack;
 
         private void InitializeComponent()
         {
             txtName = new TextBox();
-            txtPrice = new TextBox();
             btnAddItem = new Button();
             btnSave = new Button();
             listBoxItems = new ListBox();
             label1 = new Label();
-            btnBack = new Button();
             SuspendLayout();
- 
+            // 
+            // txtName
+            // 
             txtName.BackColor = SystemColors.ButtonFace;
             txtName.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
-            txtName.Location = new Point(41, 145);
+            txtName.Location = new Point(149, 117);
             txtName.Name = "txtName";
             txtName.PlaceholderText = "Product Name";
-            txtName.Size = new Size(217, 27);
+            txtName.Size = new Size(218, 27);
             txtName.TabIndex = 0;
-
-            txtPrice.BackColor = SystemColors.ButtonFace;
-            txtPrice.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
-            txtPrice.Location = new Point(41, 178);
-            txtPrice.Name = "txtPrice";
-            txtPrice.PlaceholderText = "Price";
-            txtPrice.Size = new Size(80, 27);
-            txtPrice.TabIndex = 1;
- 
+            // 
+            // btnAddItem
+            // 
             btnAddItem.BackColor = SystemColors.ButtonFace;
             btnAddItem.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
-            btnAddItem.Location = new Point(360, 306);
+            btnAddItem.Location = new Point(431, 117);
             btnAddItem.Name = "btnAddItem";
-            btnAddItem.Size = new Size(94, 40);
+            btnAddItem.Size = new Size(70, 27);
             btnAddItem.TabIndex = 2;
             btnAddItem.Text = "Add";
             btnAddItem.UseVisualStyleBackColor = false;
             btnAddItem.Click += btnAddItem_Click;
- 
+            // 
+            // btnSave
+            // 
             btnSave.BackColor = SystemColors.ButtonFace;
             btnSave.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
-            btnSave.Location = new Point(494, 306);
+            btnSave.Location = new Point(290, 301);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(94, 40);
             btnSave.TabIndex = 4;
             btnSave.Text = "Save";
             btnSave.UseVisualStyleBackColor = false;
             btnSave.Click += btnSave_Click;
-
+            // 
+            // listBoxItems
+            // 
             listBoxItems.BackColor = SystemColors.Info;
-            listBoxItems.Location = new Point(292, 145);
+            listBoxItems.Location = new Point(149, 151);
             listBoxItems.Name = "listBoxItems";
             listBoxItems.Size = new Size(352, 144);
             listBoxItems.TabIndex = 3;
-
+            // 
+            // label1
+            // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(173, 42);
+            label1.Location = new Point(163, 22);
             label1.Name = "label1";
             label1.Size = new Size(327, 62);
             label1.TabIndex = 5;
             label1.Text = "Add Groceries";
-
-            btnBack.BackColor = SystemColors.ButtonFace;
-            btnBack.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold);
-            btnBack.Location = new Point(588, 111);
-            btnBack.Name = "btnBack";
-            btnBack.Size = new Size(56, 28);
-            btnBack.TabIndex = 6;
-            btnBack.Text = "Back";
-            btnBack.UseVisualStyleBackColor = false;
-            btnBack.Click += btnBack_Click;
-   
+            // 
+            // Form2
+            // 
             BackColor = SystemColors.GradientActiveCaption;
             ClientSize = new Size(680, 380);
             Controls.Add(label1);
             Controls.Add(txtName);
-            Controls.Add(txtPrice);
             Controls.Add(btnAddItem);
             Controls.Add(listBoxItems);
             Controls.Add(btnSave);
-            Controls.Add(btnBack);
             Name = "Form2";
             Text = "Add Groceries";
             ResumeLayout(false);
@@ -108,5 +96,3 @@
         private Label label1;
     }
 }
-
-
